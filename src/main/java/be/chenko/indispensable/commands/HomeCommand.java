4ccbd701bbb05f233c7ee25b4ce1c45655adc7e5
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-19 17:22:17 +0200

    Home Command working

--- src/main/java/be/chenko/indispensable/commands/HomeCommand.java
+++ src/main/java/be/chenko/indispensable/commands/HomeCommand.java
@@ -1,5 +1,7 @@
 package be.chenko.indispensable.commands;
 
+import be.chenko.indispensable.data.UserDataHandler;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,11 +11,21 @@ public class HomeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player){
+            Player player = (Player) sender;
             // check if player has a home set
+            UserDataHandler user = new UserDataHandler(player.getPlayer().getUniqueId());
+            if (user.HasHome()){
+                player.teleport(user.getHomeLocation());
+                player.sendMessage(ChatColor.LIGHT_PURPLE + "Going home!");
+            } else {
+                player.sendMessage(ChatColor.RED + "You do not have a home yet!");
+            }
 
             // if player has a home tp player there
 
             // otherwise send player a message (You do not have a home set yet)
+
+
         }
         return true;
     }
