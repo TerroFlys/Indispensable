930211798f272b4227652844c094d7f5ead0c3d1
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-21 14:40:33 +0200

    DelWarpCommand works

--- src/main/java/be/chenko/indispensable/commands/DelWarpCommand.java
+++ src/main/java/be/chenko/indispensable/commands/DelWarpCommand.java
@@ -1,6 +1,8 @@
 package be.chenko.indispensable.commands;
 
+import be.chenko.indispensable.data.WarpDataHandler;
 import be.chenko.indispensable.util;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,10 +11,17 @@ import org.bukkit.entity.Player;
 public class DelWarpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)){
-            util.sendOnlyPlayerMessage(sender);
-            return true;
+        //this command could be used by console as no location is needed
+        if (args.length != 1){
+            sender.sendMessage(ChatColor.RED + "Wrong usage of this command.");
+            return false;
         }
+        //remove warp
+        WarpDataHandler warpDataHandler = new WarpDataHandler();
+        warpDataHandler.removeWarp(args[0]);
+
+        //remove message
+        sender.sendMessage(ChatColor.LIGHT_PURPLE + "Warp " + ChatColor.AQUA + args[0] + ChatColor.LIGHT_PURPLE + " removed.");
 
 
         return true;
