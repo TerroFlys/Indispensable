41145e426d42776eca63fdd59c6fae0bcbaabdf5
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-21 13:35:53 +0200

    Command is only for players

--- src/main/java/be/chenko/indispensable/commands/SetWarpCommand.java
+++ src/main/java/be/chenko/indispensable/commands/SetWarpCommand.java
@@ -1,12 +1,19 @@
 package be.chenko.indispensable.commands;
 
+import be.chenko.indispensable.util;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class SetWarpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)){
+            util.sendOnlyPlayerMessage(sender);
+            return true;
+        }
+
         return true;
     }
 }
--- src/main/java/be/chenko/indispensable/commands/WarpCommand.java
+++ src/main/java/be/chenko/indispensable/commands/WarpCommand.java
@@ -1,12 +1,22 @@
 package be.chenko.indispensable.commands;
 
+import be.chenko.indispensable.util;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class WarpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)){
+            util.sendOnlyPlayerMessage(sender);
+            return true;
+        }
+
+
+
+
         return true;
     }
 }
