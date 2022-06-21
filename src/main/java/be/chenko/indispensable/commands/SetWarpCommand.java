c541397a52b21191e10ca2751dae3ce30735056e
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-06-21 14:29:42 +0200

    SetWarp command works

--- src/main/java/be/chenko/indispensable/commands/SetWarpCommand.java
+++ src/main/java/be/chenko/indispensable/commands/SetWarpCommand.java
@@ -1,6 +1,9 @@
 package be.chenko.indispensable.commands;
 
+import be.chenko.indispensable.data.WarpDataHandler;
+import be.chenko.indispensable.models.Warp;
 import be.chenko.indispensable.util;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,7 +16,21 @@ public class SetWarpCommand implements CommandExecutor {
             util.sendOnlyPlayerMessage(sender);
             return true;
         }
+        //init player
+        Player player = (Player) sender;
+        //if player has not given correct amount of arguments, currently only the name
+        // in the future maybe cooldown
+        if (args.length != 1){
+            player.sendMessage(ChatColor.RED + "Wrong usage of this command.");
+            return false;
+        }
+        Warp warp = new Warp(player.getLocation(), args[0]);
 
+        WarpDataHandler warpDataHandler = new WarpDataHandler();
+        warpDataHandler.createWarp(warp);
+
+
+        player.sendMessage(ChatColor.LIGHT_PURPLE + "Set warp " + ChatColor.AQUA +args[0]+ ChatColor.LIGHT_PURPLE + ".");
         return true;
     }
 }
